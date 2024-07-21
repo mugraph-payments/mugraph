4a4619460feb720dc0a6bdf7bcc57c0b73632ebb
Cainã Costa <me@cfcosta.com>
2024-07-21 14:23:30 -0300

    fix(nix): fix cargo-risczero build on linux

--- nix/dependencies/cargo-risczero.nix
+++ nix/dependencies/cargo-risczero.nix
@@ -2,7 +2,13 @@
   openssl,
   darwin,
   mugraph,
+  lib,
+  stdenv,
 }:
+let
+  inherit (lib) optionals;
+  inherit (stdenv) isDarwin;
+in
 mugraph.dependencies.rustPlatform.buildRustPackage {
   pname = "cargo-risczero";
   version = "1.0.3";
@@ -11,8 +17,8 @@ mugraph.dependencies.rustPlatform.buildRustPackage {
   buildAndTestSubdir = "risc0/cargo-risczero";
   buildInputs = [
     openssl.dev
-    darwin.apple_sdk.frameworks.SystemConfiguration
-  ];
+
+  ] ++ optionals isDarwin [ darwin.apple_sdk.frameworks.SystemConfiguration ];
 
   doCheck = false;
   cargoLock.lockFile = "${mugraph.inputs.risc0}/Cargo.lock";
