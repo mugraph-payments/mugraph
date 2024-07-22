84ab8ecdfcbd0ac37489690ee195a34d2894f132
Cainã Costa <me@cfcosta.com>
2024-07-22 11:14:57 -0300

    chore(nix): enable darwin feature for darwin buildds

--- nix/packages/mugraph-node.nix
+++ nix/packages/mugraph-node.nix
@@ -2,9 +2,14 @@
   mugraph,
   makeWrapper,
   lib,
+  stdenv,
+  pkgs,
+  darwin,
 }:
 let
-  inherit (lib) makeBinPath;
+  inherit (stdenv) isDarwin;
+  inherit (pkgs.config) cudaSupport;
+  inherit (lib) makeBinPath optional optionals;
   inherit (mugraph.dependencies)
     rustPlatform
     rust
@@ -20,10 +25,19 @@ rustPlatform.buildRustPackage {
   env.RISC0_RUST_SRC = "${rust}/lib/rustlib/src/rust";
 
   cargoLock.lockFile = ../../Cargo.lock;
+
   nativeBuildInputs = [
     makeWrapper
     rustup-mock
   ];
+
+  buildInputs = optionals isDarwin [
+    darwin.apple_sdk.frameworks.CoreGraphics
+    darwin.apple_sdk.frameworks.Metal
+    darwin.apple_sdk.frameworks.SystemConfiguration
+  ];
+
+  buildFeatures = [ ] ++ optional isDarwin "darwin" ++ optional cudaSupport "cuda";
 
   postInstall = ''
     wrapProgram $out/bin/host \
