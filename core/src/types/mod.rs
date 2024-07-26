4cf8e9347342150ad6d41e89f6e57b2547935161
Cainã Costa <me@cfcosta.com>
2024-07-26 14:41:19 -0300

    chore(core): clean up types a little more

--- core/src/types/mod.rs
+++ core/src/types/mod.rs
@@ -1,6 +1,11 @@
+#[cfg(test)]
+use crate::testing::*;
+#[cfg(test)]
+use proptest::{collection::vec, prelude::*};
+#[cfg(test)]
+use test_strategy::Arbitrary;
+
 pub mod delegate;
-pub mod note;
-pub mod request;
 
 pub use curve25519_dalek::traits::*;
 
@@ -12,10 +17,57 @@ pub type SecretKey = Scalar;
 pub type CompressedPoint = curve25519_dalek::ristretto::CompressedRistretto;
 
 #[derive(Debug, Clone)]
-#[cfg_attr(test, derive(test_strategy::Arbitrary))]
+#[cfg_attr(test, derive(Arbitrary))]
 pub struct Signature {
-    #[cfg_attr(test, strategy(crate::testing::point()))]
+    #[cfg_attr(test, strategy(point()))]
     pub r: Point,
-    #[cfg_attr(test, strategy(crate::testing::scalar()))]
+    #[cfg_attr(test, strategy(scalar()))]
     pub s: Scalar,
+}
+
+#[derive(Debug)]
+#[cfg_attr(test, derive(Arbitrary))]
+pub struct Note {
+    /// The ID for the asset in the Cardano blockchain
+    pub asset_id: Hash,
+
+    /// The amount included in this note
+    pub amount: u128,
+
+    /// Unblinded signature from the server from this note creation
+    ///
+    /// Equivalent to C in the protocol, returned by the server after minting or swapping
+    /// assets.
+    pub signature: Signature,
+}
+
+#[derive(Debug)]
+#[cfg_attr(test, derive(Arbitrary))]
+pub struct UnblindedNote {
+    pub asset_id: Hash,
+    pub amount: u128,
+    pub nonce: Hash,
+}
+
+#[derive(Debug, Clone)]
+#[cfg_attr(test, derive(Arbitrary))]
+pub struct Proof;
+
+#[derive(Debug, Clone)]
+#[cfg_attr(test, derive(Arbitrary))]
+pub struct Commit {}
+
+#[derive(Debug, Clone)]
+#[cfg_attr(test, derive(Arbitrary))]
+pub struct Swap {
+    #[cfg_attr(test, strategy(vec(any::<Signature>(), 0..=16)))]
+    pub inputs: Vec<Signature>,
+
+    /// The blinded secrets to be signed by the delegate.
+    ///
+    /// Corresponds to B' in the protocol.
+    #[cfg_attr(test, strategy(vec(point(), 0..=16)))]
+    pub outputs: Vec<Point>,
+
+    pub proof: Proof,
 }
