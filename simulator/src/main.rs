f726a80a09d402695ab80a1c76ecd0251e0f37e9
Cainã Costa <me@cfcosta.com>
2024-08-10 18:08:39 -0300

    feat(simulator): run one task per available physical core

--- simulator/src/main.rs
+++ simulator/src/main.rs
@@ -1,15 +1,51 @@
-use color_eyre::eyre::Result;
+use std::thread::{self, JoinHandle};
+
+use color_eyre::eyre::{ErrReport, Result};
 use mugraph_simulator::{Config, Simulator};
+use tokio::runtime::Builder;
 
-#[tokio::main]
-async fn main() -> Result<()> {
+fn main() -> Result<()> {
     color_eyre::install()?;
+    let cores = core_affinity::get_core_ids().unwrap();
+    let mut handles = vec![];
 
-    let config = Config::default();
-    let mut simulator = Simulator::new().setup(config).await?;
+    for i in 0..num_cpus::get_physical() {
+        let core = cores[i];
+        let handle: JoinHandle<Result<(), ErrReport>> = thread::spawn(move || {
+            core_affinity::set_for_current(core);
 
-    loop {
-        simulator.tick().await?;
+            let rt = Builder::new_current_thread().enable_all().build()?;
+            rt.block_on(async move {
+                let config = Config::default();
+                let mut simulator = Simulator::new().setup(config).await?;
+
+                loop {
+                    simulator.tick().await?;
+                }
+
+                #[allow(unreachable_code)]
+                Ok::<_, ErrReport>(())
+            })?;
+
+            #[allow(unreachable_code)]
+            Ok::<_, ErrReport>(())
+        });
+
+        handles.push(handle);
+    }
+
+    for handle in handles {
+        match handle.join() {
+            Ok(Ok(())) => continue,
+            Ok(Err(e)) => {
+                eprintln!("Thread failed: {:?}", e);
+                std::process::exit(1);
+            }
+            Err(e) => {
+                eprintln!("Thread failed: {:?}", e);
+                std::process::exit(1);
+            }
+        }
     }
 
     #[allow(unreachable_code)]
