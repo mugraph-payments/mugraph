b50fd39a7fa006b570d801268bdbbbf9d0035fb1
Cainã Costa <me@cfcosta.com>
2024-10-22 17:14:54 -0300

    feat: remove old implementation

--- core/src/testing.rs
+++
@@ -1,6 +0,0 @@
-use proptest::prelude::*;
-use rand::{prelude::*, rngs::StdRng};
-
-pub fn rng() -> impl Strategy<Value = StdRng> {
-    any::<[u8; 32]>().prop_map(StdRng::from_seed)
-}
--- core/src/types/request/v0.rs
+++
@@ -1,9 +0,0 @@
-use serde::{Deserialize, Serialize};
-use test_strategy::Arbitrary;
-
-#[derive(Debug, Clone, Serialize, Deserialize, Arbitrary)]
-#[serde(tag = "m", content = "p")]
-pub enum Request {
-    #[serde(rename = "transaction")]
-    Transaction(crate::types::Transaction),
-}
