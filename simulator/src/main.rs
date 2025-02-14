a7571de687794816fe298e9e5d206d28cc267337
Cainã Costa <me@cfcosta.com>
2025-02-14 13:50:11 -0300

    chore: move to mimalloc instead of jemalloc

--- simulator/src/main.rs
+++ simulator/src/main.rs
@@ -1,5 +1,5 @@
 #[global_allocator]
-static GLOBAL: tikv_jemallocator::Jemalloc = tikv_jemallocator::Jemalloc;
+static GLOBAL: mimalloc::MiMalloc = mimalloc::MiMalloc;
 
 use std::{
     collections::VecDeque,
